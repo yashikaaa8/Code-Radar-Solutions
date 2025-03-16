29dc21a926a3ded127d48fae9a568badf799ee7c
Yashika <yashikaaa.lakhera08@gmail.com>
2025-03-16 21:26:18 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -3,22 +3,16 @@
  int main() {
      int n;
          scanf("%d", &n);
- 
-             
-                 for (int i = 1; i <= n; i++) {
-                         
-                                 for (int j = 1; j <= n - i; j++) {
+         for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
                                              printf(" ");
                                                      }
-                                                            
-                                                                     for (int j = 1; j <= 2 * i - 1; j++) {
+                                                     for (int j = 1; j <= 2 * i - 1; j++) {
                                                                                  printf("*");
                                                                                          }
                                                                                                  printf("\n");
                                                                                                      }
- 
-                                                                                                         
-                                                                                                             for (int i = n - 1; i >= 1; i--) {
+                                                                                                     for (int i = n - 1; i >= 1; i--) {
                                                                                                                      
                                                                                                                              for (int j = 1; j <= n - i; j++) {
                                                                                                                                          printf(" ");
