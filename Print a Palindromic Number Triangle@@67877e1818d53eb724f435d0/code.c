9ea72d0771765c215acd6c045386b87c996ac1f9
Yashika <yashikaaa.lakhera08@gmail.com>
2025-03-16 22:37:30 +0530

    Solution to Print a Palindromic Number Triangle

---
+++ Print a Palindromic Number Triangle@@67877e1818d53eb724f435d0/code.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+ 
+ int main() {
+     int n;
+         scanf("%d", &n);
+ 
+             for (int i = 1; i <= n; i++) {
+                     // Print leading spaces
+                             for (int j = 1; j <= n - i; j++) {
+                                         printf(" ");
+                                                 }
+ 
+                                                         // Print increasing numbers
+                                                                 for (int j = 1; j <= i; j++) {
+                                                                             printf("%d", j);
+                                                                                     }
+ 
+                                                                                             // Print decreasing numbers
+                                                                                                     for (int j = i - 1; j >= 1; j--) {
+                                                                                                                 printf("%d", j);
+                                                                                                                         }
+ 
+                                                                                                                                 printf("\n");
+                                                                                                                                     }
+ 
+                                                                                                                                         return 0;
+                                                                                                                                         }
+ 
